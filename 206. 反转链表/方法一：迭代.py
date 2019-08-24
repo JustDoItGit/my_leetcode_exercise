5a09d493951ae33b16514297f253ec1ee385528d
cool <cool_kj@126.com>
2019-08-24 23:27:24 +0800

    反转链表补充

--- 206. 反转链表/方法一：迭代.py
+++ 206. 反转链表/方法一：迭代.py
@@ -2,5 +2,11 @@ class Solution:
     def reverseList(self, head):
         cur, prev = head, None
         while cur:
+            """
+            nextTemp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nextTemp
+            """
             cur.next, prev, cur = prev, cur, cur.next
         return prev
