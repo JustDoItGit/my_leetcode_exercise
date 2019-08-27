f311d36d2ba37e6e362b93e2ad33f5d386f1d95c
cool <cool_kj@126.com>
2019-08-27 23:20:47 +0800

    两两交换链表中的节点

--- 24. 两两交换链表中的节点/常规解法.py
+++ 24. 两两交换链表中的节点/常规解法.py
@@ -4,6 +4,7 @@ class Solution:
         while pre.next and pre.next.next:
             a = pre.next
             b = a.next
-            pre.next, b.next, a.next = b, a, b.next
+            # 暂时不明白，为啥要pre.next 指向 b
+            pre.next, b.next, a.next = b, a, b.next  # 这里的一次性赋值，动作全部都是指向
             pre = a
-        return self.next
+        return self.next  # 结果要求返回的是链表第一个元素head
