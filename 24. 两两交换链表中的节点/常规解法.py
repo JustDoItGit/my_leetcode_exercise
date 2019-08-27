18a38f9249bac1718b0b939f7e4e2af211cf1e83
cool <cool_kj@126.com>
2019-08-27 23:23:30 +0800

    pre.next 指向 b， 防止链表丢掉两两交换后的第一个节点

--- 24. 两两交换链表中的节点/常规解法.py
+++ 24. 两两交换链表中的节点/常规解法.py
@@ -4,7 +4,7 @@ class Solution:
         while pre.next and pre.next.next:
             a = pre.next
             b = a.next
-            # 暂时不明白，为啥要pre.next 指向 b
+            # pre.next 指向 b， 防止链表丢掉两两交换后的第一个节点
             pre.next, b.next, a.next = b, a, b.next  # 这里的一次性赋值，动作全部都是指向
             pre = a
         return self.next  # 结果要求返回的是链表第一个元素head
